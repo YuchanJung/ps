b7101e0fcef071734b5597c601faf13eef42bb8c
YuchanJung <yuchandev@gmail.com>
2024-05-24 23:40:26 +0900

    etc

---
+++ .etc/삼성 SW역량테스트 기출/2024/우주선 탐사/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+ 
+struct Ship {
+    int x, y, d; // 중심 x, 중심 y, 출구 방향
+};
+
+int T, N, M, K; // tc, 행, 열, 우주선 개수
+
+void init() {}
+bool canGoDown(int x, int y) {}
+bool canGoLeft(int x, int y) {}
+bool canGoRight(int x, int y) {}
+void ship_arrive(int idx) {}
+void bfs_init() {}
+void bfs() {}
+void get_score() {}
+ 
+int main() {
+
+}
